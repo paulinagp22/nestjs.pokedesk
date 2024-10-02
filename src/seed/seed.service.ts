0e0d39993f7e47169fde88ebeb29dde74ef135df
Paulina Guerrero <paaulinagp@gmail.com>
2024-10-02 00:40:08 -0600

    feat: se crea un seed para agregar todos los pokemones

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokemonResponse } from './interfaces/pokemon-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokemonResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=50',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
     const pokemons = data.results.map(({ name, url }) => {
@@ -21,6 +29,8 @@ export class SeedService {
       };
     });
 
-    return pokemons;
+    await this.pokemonModel.insertMany(pokemons);
+
+    return 'Seed executed';
   }
 }
