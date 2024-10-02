efdc4f64f792c1f727b728b5b3d3d4bad49ca3f6
Paulina Guerrero <paaulinagp@gmail.com>
2024-10-01 21:32:46 -0600

    feat: se agrega api de pokemon

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Prefijo global para que las llamadas a la API sean /api/...
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
