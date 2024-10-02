b5eb5393cdf77b547a37fc16cdb53e25ec4dcaf2
Paulina Guerrero <paaulinagp@gmail.com>
2024-10-01 22:23:07 -0600

    feat: onstalando mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    // Configuración para servir archivos
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    // Referencia a la base de datos
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-pokemon'),
     PokemonModule,
   ],
   controllers: [],
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,22 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  // id: string; // Se comenta porque Mongoose crea un id automáticamente
+
+  // Se añaden las propiedades para que sea unico y se pueda indexar
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  pokemonId: number;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
