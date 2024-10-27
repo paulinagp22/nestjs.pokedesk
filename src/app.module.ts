a9c8254173cac88082f37b5ed004e018d82313bc
Paulina Guerrero <paaulinagp@gmail.com>
2024-10-27 15:19:17 -0600

    feat: adding env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    // Configuración para cargar variables de entorno
+    ConfigModule.forRoot(),
     // Configuración para servir archivos
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
     // Referencia a la base de datos
-    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule,
@@ -21,4 +24,6 @@ import { SeedModule } from './seed/seed.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+}
